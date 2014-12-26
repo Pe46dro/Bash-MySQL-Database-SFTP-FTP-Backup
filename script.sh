9db525d312f9890e39742fefaf980c83b37165ba
Emilien Devos <unixfox@users.noreply.github.com>
2014-12-27 00:43:11 +0100

    Add possibility to upload with sftp and code cleanup.
    Code to be tested before production.

--- script.sh
+++ script.sh
@@ -1,10 +1,27 @@
-#!/bin/sh
+#!/bin/bash
 
 # Linux MySQL Database FTP Backup Script
 # Version: 1.0
 # Script by: Pietro Marangon
 # Skype: pe46dro
 # Email: pietro.marangon@gmail.com
+# SFTP function by unixfox
+
+create_backup() {
+  backup_path="/root"
+
+  umask 177
+
+  mysqldump --user=$user --password=$password --host=$host $db_name > $db_name-$d.sql
+
+  FILE="$db_name-$d.sql"
+  echo 'Backup Complete'
+}
+
+clean_backup() {
+  rm -f $backup_path/$db_name-$date.sql
+  echo 'Local Backup Removed'
+}
 
 ########################
 # Edit Below This Line #
@@ -38,15 +55,9 @@ d=$(date --iso)
 
 if [ $TYPE -eq 1 ]
 then
-backup_path="/root"
 
-umask 177
+create_backup
 
-mysqldump --user=$user --password=$password --host=$host $db_name > $db_name-$d.sql
-
-FILE="$db_name-$d.sql"
-
-echo 'Backup Complete'
 ftp -n -i $SERVER <<EOF
 user $USERNAME $PASSWORD
 binary
@@ -54,13 +65,18 @@ mput $FILE $REMOTDIR/$FILE
 quit
 EOF
 echo 'Remote Backup Complete'
-rm -f $backup_path/$db_name-$date.sql
-echo 'Local Backup Removed'
+clean_backup
 
 elif [ $TYPE -eq 2 ]
 then
-echo "Please use FTP, SFTP isn't supported"
+cd $REMOTDIR
+sftp $USERNAME@$SERVER
+$PASSWORD
+put $FILE
+exit
+echo 'Remote Backup Complete'
+clean_backup
 else
-echo "Please select a valid type"
+echo 'Please select a valid type'
 fi
 #END
